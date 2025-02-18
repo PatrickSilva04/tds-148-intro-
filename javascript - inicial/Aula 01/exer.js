a7606c8a444f47ccbea910cdfde1d55acaf278f3
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-02-18 15:29:13 -0300

    java intro

---
+++ javascript - inicial/Aula 01/exer.js
@@ -0,0 +1,40 @@
+// EXERCICIOS variaveis
+
+//Declara uma variavel com a sua idade
+
+const idade= 21
+
+//Declarar uma variavel com a sua comida preferida e atribuir um valor
+
+var comida
+comida ="lasanha"
+
+// Declarar uma variavel e nao dar valor pra ela, sendo undefined
+
+var qualquer
+
+// Declarar 5 variaveis diferentes sem valores
+
+var var1,var2,var3,var4,var5
+
+// Declarar uma variavel contendo uma string
+
+var a="qualquer coisa"
+
+// Declare uma varivel contendo um numero dentro de uma string
+
+var a="Eu tenho 21 anos"
+
+// Declare uma variavel com seu nome, e outra com sobrenome, e concatene os dois, depois faça um template string dos 2
+
+const nome = patrick
+const sobrenome = silva
+console.log("meu nome é"+nome,+sobrenomenome)
+console.log(`meu nome é ${nome} ${sobrenome}`)
+
+// Coloque a seguinte frase em uma variavel: It´s time
+
+var a="It´s time"
+
+// verifique o tipo de todas as variaveis criadas
+console.log(typeof idade, typeof qualquer,typeof idade)
